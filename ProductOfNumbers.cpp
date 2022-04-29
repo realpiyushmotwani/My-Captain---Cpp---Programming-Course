dfa2cc1db6c65404927da2c28ba61f8e3118c4d0
Piyush Motwani <100508072+realpiyushmotwani@users.noreply.github.com>
2022-04-29 19:46:10 +0530

    Create ProductOfNumbers.cpp

---
+++ ProductOfNumbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+// program to calculate product of 2 numbers
+using namespace std;
+main()
+{
+    double num1,num2;
+    cout<<"Enter 2 numbers"<<"\n";
+    cin>>num1;
+    cin>>num2;
+    cout<<"The product is "<<num1*num2;
+
+}
