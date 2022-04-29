fe6e84e9836d02d2d5c1459ccd0fc4a371056b4e
Piyush Motwani <100508072+realpiyushmotwani@users.noreply.github.com>
2022-04-29 19:41:07 +0530

    Create SizeOfDataTypes.cpp

---
+++ SizeOfDataTypes.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+// program to calculate size of various data types using sizeof() built in function
+using namespace std;
+main()
+{
+    cout<<"size of int is "<<sizeof(int)<<endl;
+    cout<<"size of float is "<<sizeof(float)<<endl;
+    cout<<"size of double is "<<sizeof(double)<<endl;
+    cout<<"size of char is "<<sizeof(char)<<endl;
+}
